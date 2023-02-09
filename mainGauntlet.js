cc38321e373b1686a7ffe4556219827bfc1314a0
Chris <123058745+Mossbug@users.noreply.github.com>
2023-02-09 00:48:24 -0500

    Add files via upload

---
+++ mainGauntlet.js
@@ -0,0 +1,33 @@
+
+function gauntletRedirect() {
+    var gauntletCode = document.getElementById('gauntletCode').value;
+    if (gauntletCode == "val") {
+        //When launching on github, replace with this line: 'https://mossbug.github.io/erdin.github.io/val.html'
+        location.replace('file:///C:/Users/Chris/Desktop/ErdinLanding/val.html')
+    }
+
+    var gauntletCode = document.getElementById('gauntletCode').value;
+    if (gauntletCode == "germaine") {
+        //When launching on github, replace with this line: 'https://mossbug.github.io/erdin.github.io/germaine.html'
+        location.replace('file:///C:/Users/Chris/Desktop/ErdinLanding/germaine.html')
+    }
+
+    var gauntletCode = document.getElementById('gauntletCode').value;
+    if (gauntletCode == "cadenza") {
+        //When launching on github, replace with this line: 'https://mossbug.github.io/erdin.github.io/cadenza.html'
+        location.replace('file:///C:/Users/Chris/Desktop/ErdinLanding/cadenza.html')
+    }
+
+    var gauntletCode = document.getElementById('gauntletCode').value;
+    if (gauntletCode == "shevaria") {
+        //When launching on github, replace with this line: 'https://mossbug.github.io/erdin.github.io/shevaria.html'
+        location.replace('file:///C:/Users/Chris/Desktop/ErdinLanding/shevaria.html')
+    }
+
+    var gauntletCode = document.getElementById('gauntletCode').value;
+    if (gauntletCode == "esic") {
+        //When launching on github, replace with this line: 'https://mossbug.github.io/erdin.github.io/esic.html'
+        location.replace('file:///C:/Users/Chris/Desktop/ErdinLanding/esic.html')
+    }
+}
+
